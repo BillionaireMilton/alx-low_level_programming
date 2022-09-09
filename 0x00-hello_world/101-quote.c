8e42b2edcc3fd90cc265fd3c0dfbdb6c306c65d9
BillionaireMilton <henrymilton003@gmail.com>
2022-09-09 07:46:32 -0700

    fixed my codes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-#include <unistd.h>
-
 /**
- *
- *  *main - main
- *
- *   * Return:1
- *
- *    */
-
+ * main - prints to string
+ * Description: prints text without puts
+ * Return: 1
+ */
 int main(void)
-
 {
-
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
