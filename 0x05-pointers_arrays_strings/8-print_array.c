0db39d5a60f487f9218ab9cfc71e4846f8859d40
BillionaireMilton <henrymilton003@gmail.com>
2022-09-20 03:30:07 +0100

    fixed print array issues

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,25 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers.
- *
- * @n: elements parameter input
- * @a: string parameter input
- *
- * Return: Nothing
+ * print_array - Prints input number count
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
 */
-
 void print_array(int *a, int n)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < n; ++i)
+	for (index = 0; index < n; index++)
 	{
-		if (i != (n - 1))
-			printf("%d, ", a[i]);
-		else
-			printf("%d", a[i]);
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
 	}
+
 	printf("\n");
 }
+
