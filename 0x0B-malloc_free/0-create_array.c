f5253cc8f8e6355e313debb09a432f259d82134f
BillionaireMilton <henrymilton003@gmail.com>
2022-10-06 15:34:59 +0100

    corrected spelling

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 /**
  * create_array - creates an array of chars.
  * @size: size of the array.
- * @c: storaged char
+ * @c: storage char
  * Return: pointer of an array of chars
  */
 char *create_array(unsigned int size, char c)
